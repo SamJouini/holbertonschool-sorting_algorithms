24fafbb16c299e70616d10a902c30c0c1cfcf3a3
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-10 14:21:55 +0200

    without swap count

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned long int i;
 	unsigned long int j;
-	int temp, swapped;
+	int temp;
 
 	if (array == NULL || size < 2)
 	{
@@ -20,7 +20,6 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -28,13 +27,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
 				print_array(array, size);
 			}
-		}
-		if (swapped == 0)
-		{
-			break;
 		}
 	}
 }
