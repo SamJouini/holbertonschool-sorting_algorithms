16b8ad9011a6121f69cc1a1c07dcd77a6b0a56e7
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-10 14:30:23 +0200

    back the 1st try

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,9 +9,9 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
-	size_t j;
-	int temp;
+	unsigned long int i;
+	unsigned long int j;
+	int temp, swapped;
 
 	if (array == NULL || size < 2)
 	{
@@ -20,15 +20,22 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = 1;
 				print_array(array, size);
 			}
+		}
+		
+		if (swapped == 0)
+		{
+			break;
 		}
 	}
 }
