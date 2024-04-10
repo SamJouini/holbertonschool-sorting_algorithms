9f0e01da74019f58add8a320484b4de014f9afec
SamJouini <samantha.u.h.jouini@gmail.com>
2024-04-10 14:23:29 +0200

    without swap count

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,7 +20,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
